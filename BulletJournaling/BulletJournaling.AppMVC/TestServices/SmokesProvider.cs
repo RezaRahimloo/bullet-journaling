827286fe5fbca88f4b8a1fa4b96147c533ecde34
Reza Rahimloo <mr.rezarahimlou@gmail.com>
2022-07-03 15:01:46 +0430

    Added improvements to smoking page UI

--- BulletJournaling/BulletJournaling.AppMVC/TestServices/SmokesProvider.cs
+++ BulletJournaling/BulletJournaling.AppMVC/TestServices/SmokesProvider.cs
@@ -6,23 +6,27 @@ public class SmokesProvider
     private List<SmokingModel> _smokings = new();
     public List<SmokingModel> GetSmokings()
     {
-        DateTime sixteenWeeksAgo = DateTime.Now.AddDays(-111);
-        for(int i =0; i < 112; i++)
+        for (int i = -3; i < 1; i++)
         {
-            if(i % 4 == 0)
+            DateOnly fourMonthsAgo = DateOnly.FromDateTime(DateTime.Now).AddMonths(i);
+            DateOnly lastFourMonths = new(fourMonthsAgo.Year, fourMonthsAgo.Month, 1);
+            for (int j = 0; j < DateTime.DaysInMonth(lastFourMonths.Year, lastFourMonths.Month); j++)
             {
-                _smokings?.Add(new SmokingModel 
-                { 
-                    DidSmoke = false
-                });
-            }
-            else
-            {
-                _smokings?.Add(new SmokingModel
+                if(j%4 == 0)
                 {
-                    DidSmoke = true,
-                    Number = new Random().Next(1, 13)
-                });
+                    _smokings?.Add(new SmokingModel 
+                    { 
+                        DidSmoke = false
+                    });
+                }
+                else
+                {
+                    _smokings?.Add(new SmokingModel
+                    {
+                        DidSmoke = true,
+                        Number = new Random().Next(1, 13)
+                    });
+                }
             }
         }
         return _smokings;
