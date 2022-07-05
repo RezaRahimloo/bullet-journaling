3aa3936980c1ba63fe788685fdaa8927744c3de8
Reza Rahimloo <mr.rezarahimlou@gmail.com>
2022-07-05 13:06:33 +0430

    added improvements to form for adding logs

--- BulletJournaling/BulletJournaling.AppMVC/wwwroot/js/log/log.js
+++ BulletJournaling/BulletJournaling.AppMVC/wwwroot/js/log/log.js
@@ -1,8 +1,18 @@
 let mainActivitiesShow = document.getElementById("day-activites");
+let openFormButton = document.getElementById("add-log");
+let form = document.querySelector('form#add-log-form');
+let formGreyShade = document.querySelector('.grey-shade-form');
 document.querySelectorAll('div.day.log[data-did="true"]').forEach( element => {
     element.addEventListener('focus', e => focusedDay(e))
 })
-
+openFormButton.addEventListener('click', () => {
+    form.classList.toggle("open");
+    formGreyShade.classList.toggle("active");
+})
+formGreyShade.addEventListener('click', () => {
+    formGreyShade.classList.toggle("active");
+    form.classList.remove("open");
+})
 function focusedDay(event){
     let elm = event.target;
     mainActivitiesShow.innerHTML = elm.children[1].innerHTML;
