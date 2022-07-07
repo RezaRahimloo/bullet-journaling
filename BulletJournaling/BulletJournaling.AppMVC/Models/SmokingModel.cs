5bf40cb93a54c7fac69b9ebe251b0cb703252227
Reza Rahimloo <mr.rezarahimlou@gmail.com>
2022-07-07 10:37:41 +0430

    Added Smoking basic partial view

--- BulletJournaling/BulletJournaling.AppMVC/Models/SmokingModel.cs
+++ BulletJournaling/BulletJournaling.AppMVC/Models/SmokingModel.cs
@@ -5,6 +5,7 @@ namespace BulletJournaling.AppMVC.Models
     public class SmokingModel
     {
         [Required]
+        [Display(Name ="Did you smoke today?")]
         public bool DidSmoke { get; set; }
         public int Number { get; set; } = 0;
         public DateOnly Date { get; set; } 
