0abeaf0daef02ee8e5af4d906c00ce3fe95b1931
Reza Rahimloo <mr.rezarahimlou@gmail.com>
2022-07-19 12:39:22 +0430

    Improved MBA UI and added inputs and buttons for CRUD Actions

--- BulletJournaling/BulletJournaling.AppMVC/wwwroot/js/Mba/mba.js
+++ BulletJournaling/BulletJournaling.AppMVC/wwwroot/js/Mba/mba.js
@@ -1,9 +1,22 @@
+let openFormButton = document.getElementById("add-class");
+let form = document.querySelector('#log-form-wrapper');
+let formGreyShade = document.querySelector('.grey-shade-form');
+
 let mainActivitiesShow = document.getElementById("day-activites");
 document.querySelectorAll('div.day.log[data-did="true"]').forEach( element => {
     element.addEventListener('focus', e => focusedDay(e))
 })
-
 function focusedDay(event){
     let elm = event.target;
     mainActivitiesShow.innerHTML = elm.children[1].innerHTML;
 }
+
+openFormButton.addEventListener('click', () => {
+    form.classList.toggle("open");
+    formGreyShade.classList.toggle("active");
+})
+formGreyShade.addEventListener('click', () => {
+    formGreyShade.classList.toggle("active");
+    form.classList.remove("open");
+})
+
